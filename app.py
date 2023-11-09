b86aa9222ff128c9c80454a874352f07cb889f9d
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2023-11-09 16:00:28 +0000

    Frontend Completed

--- app.py
+++ app.py
@@ -4,9 +4,27 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def function():
+def homepage():
   return render_template('homepage.html')
 
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+  return render_template('register.html')
+
+
+@app.route("/contactus")
+def contactus():
+  return render_template('contactus.html')
+
+@app.route("/partners")
+def partners():
+  return render_template('partners.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
